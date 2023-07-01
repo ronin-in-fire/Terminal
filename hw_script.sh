e2e30ec06bcbe2f2917e7ca8f54d2a0277f79b66
Rustam <ronin.in.fire@gmail.com>
2023-07-01 11:11:33 +0300

    script

---
+++ hw_script.sh
@@ -0,0 +1,8 @@
+cd Terminal
+mkdir folder_1 folder_2 folder_3
+cd folder_3
+touch file_1.txt file_2.txt file_3.txt file_4.json file_5.json
+mkdir dir_1 dir_2 dir_3
+ls -l
+mv file_3.txt file_5.json /d/33_Group/Terminal/folder_3/dir_2/
+
